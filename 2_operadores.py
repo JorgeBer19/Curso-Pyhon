5de25d76005e6a0066f57be62a9e4680bb4db6ce
Jorge_Bernal_Liarte <5885998@alu.murciaeduca.es>
2024-11-13 19:54:22 +0100

    Subida Ficheros2
    21/11/2024

---
+++ 2_operadores.py
@@ -0,0 +1,56 @@
+###Operadores Aritméticos ###
+
+#Operaciones de enteros
+#Suma de dos números con variables
+numero1=1
+Numero2=2
+print("Suma de dos numero")
+print(numero1+Numero2)
+#Suma
+print(numero1+Numero2)
+#Resta
+print(numero1-Numero2)
+#Multiplicacion
+print(numero1*Numero2)
+#Division
+print(numero1/Numero2)
+#Modulo
+print(numero1%Numero2)
+#Division entera "Sin decimales"
+print(numero1//Numero2)
+#Exponente
+print(numero1**Numero2)
+
+#Combinar operaciones
+print(2**3-7/1//4)
+
+#Operaciones con texto
+print("Hola " + "Mundo")
+
+#print("Hola " + 5) Tira un error porque 5 es número y hola es string
+
+print("Hola " + str(5))
+
+print("---- " * 7)
+
+print("Hola" * 7)
+
+#print("Hola" ** 7) Da error
+
+print("Hola" *(3**5)) # Doble ** es elevar
+
+my_float= 2.5 * 2 #Coma flotante coma =.
+print("Hola " * int(my_float)) #No se puede multiplicar numeros de coma plotante con texto
+
+print(type(my_float))
+
+### Operadores Comparativos###
+
+print(3 > 4)
+print(3 < 4)
+print(3 >=4)
+print(3 <=4)
+print(3 ==4)
+print(3 !=4)
+
+print(numero1 !=Numero2)
